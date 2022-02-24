51d439de1427488ae86f44615b85925bed1fca19
patipan-patisampita <patipan.patisampita@gmail.com>
2022-02-24 11:47:02 +0700

    update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
+import 'contact.dart';
+import 'profile.dart';
+
+class HomeScreen extends StatelessWidget {
   static String id = '/home';
-  const HomeScreen({Key? key}):super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
@@ -28,24 +31,32 @@ class HomeScreen extends StatelessWidget{
                 accountEmail: Text("mark@facebok.com"),
                 currentAccountPicture: CircleAvatar(
                   // child: Icon(Icons.android),
-                  backgroundImage: NetworkImage("https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg"),
+                  backgroundImage: NetworkImage(
+                      "https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg"),
                 ),
               ),
             ),
             ListTile(
-              title: const Text("Home",style: TextStyle(color: Colors.amber,fontSize: 15)),
-              leading: Icon(Icons.home,color:Colors.indigo,size: 30),
+              title: const Text("Home", style: TextStyle(color: Colors.amber, fontSize: 15)),
+              leading: Icon(Icons.home, color: Colors.indigo, size: 30),
               onTap: () {
-               Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
               },
             ),
             ListTile(
-              title: const Text("Profile"),
-              onTap: () {},
+              title: const Text("Profile", style: TextStyle(color: Colors.indigo, fontSize: 15)),
+              leading: Icon(Icons.ac_unit, color: Colors.indigo, size: 30),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ProfileScreen()));
+              },
             ),
             ListTile(
-              title: const Text("Contact"),
-              onTap: () {},
+              title: const Text("Contact", style: TextStyle(color: Colors.indigo, fontSize: 15)),
+              leading: Icon(Icons.call, color: Colors.indigo, size: 30),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ContactScren()));
+              },
             ),
             ListTile(
               title: const Text("Login"),
