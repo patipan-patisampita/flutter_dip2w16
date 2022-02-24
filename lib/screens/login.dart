04f6de90399812945f87ad58bf44d5066e040e9a
patipan-patisampita <patipan.patisampita@gmail.com>
2022-02-24 14:45:41 +0700

    update

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   static String id = '/login';
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +20,58 @@ class LoginScreen extends StatelessWidget {
         title: Text("Login Screen"),
         backgroundColor: Colors.purple,
       ),
-      body: Text("Login Screen"),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value == "") {
+                    return "Field is required";
+                  }
+                  return null;
+                },
+                onSaved: (value){
+                  print("Name field is Saved");
+                },
+                controller: nameController,
+                decoration: InputDecoration(
+                  labelText: "Name",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 15),
+              TextFormField(
+                validator: (value) {
+                  if (value!.length < 3) {
+                    return "Password shoule be more than 3 characters";
+                  }
+                  return null;
+                },
+                onSaved: (value){
+                  print("Password field is Saved");
+                },
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formkey.currentState!.validate() == true) {
+                      _formkey.currentState!.save();
+                      print("Form Submitted Successfully");
+                    }
+                  },
+                  child: Text("Submit"))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
